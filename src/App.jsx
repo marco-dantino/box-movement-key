ad82cbcebd335bbca34e84b7c103a4c37755297c
Marco D' Antino <marco.dantino@colidevs.com>
2026-04-17 20:32:35 -0300

    canvas

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 function App() {
+  
 
   return (
-    <>
-      <h1 className="text-3xl font-bold underline">Hola mundo</h1>
-    </>
+    <div contentEditable={"true"} onKeyDown={e => {console.log}} className="bg-red-500 absolute w-32 h-32"></div> 
   )
 }
+{/* <input onKeyDown={ e => {console.log("KeyDown", e);} } type="text" /> */}
 
 export default App
