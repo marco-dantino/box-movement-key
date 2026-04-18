4c81dc5d2967f9c9a20550a495f94dd4eeb5cd4c
Marco D' Antino <marco.dantino@colidevs.com>
2026-04-18 00:32:22 -0300

    change shape with geometry and speed

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,25 @@ import { useState } from "react"
 //x horizontal y verttical 
 
 function App() {
-  const [coordinates, setCoordinates] = useState({x: 150, y: 150 })
-  let movement = 10;
+  const [coordinates, setCoordinates] = useState({ x: window.innerWidth/2 - 64, y: window.innerHeight/2 - 64 });
+  const [movement, setMovement] = useState(10);
   
+  let shapeCircle = 9999;
+  let shapeSquare = 0;
+  const [shape, setShape] = useState({ geometry: shapeSquare, color: "red" });
+  
+  function handleClickShape(){
+    if(shape.geometry == shapeSquare){
+      setShape({ geometry: shapeCircle, color: "blue" });
+      setMovement(1);
+    }
+
+    if(shape.geometry == shapeCircle){
+      setShape({ geometry: shapeSquare, color: "red" });
+      setMovement(50);
+    }
+  }
+
   function handleOnKeyDown(eventCode){
     switch(eventCode){
       case "ArrowUp":
@@ -26,7 +42,7 @@ function App() {
     }
   }
   return (
-    <div contentEditable onKeyDown={e => handleOnKeyDown(e.code) } style={{top: coordinates.y, right: coordinates.x, transition: "all 0.2s linear"}} className="bg-red-500 absolute w-32 h-32"></div> 
+    <div onClick={handleClickShape} tabIndex={1} onKeyDown={e => handleOnKeyDown(e.code) } style={{top: coordinates.y,  right: coordinates.x, borderRadius: shape.geometry, backgroundColor: shape.color, transition: "all 0.2s linear"}} className=" absolute w-32 h-32"></div> 
   )
 }
 {/* <input onKeyDown={ e => {console.log("KeyDown", e);} } type="text" /> */}
