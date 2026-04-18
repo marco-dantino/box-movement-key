cd5ceab951c3ed2a6002a4391699c925aa142346
Marco D' Antino <marco.dantino@colidevs.com>
2026-04-17 21:12:42 -0300

    cases key.code

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,22 @@
-function App() {
-  
+import { useState } from "react"
 
+function App() {
+  const [coordinates, setCoordinates] = useState({x: 0, y: 0 })
+
+  function handleOnKeyDown(eventCode){
+    switch(eventCode){
+      case 'ArrowUp':
+        break;
+      case 'ArrowDown':
+        break;
+      case 'ArrowRight':
+        break;
+      case 'ArrowLeft':
+        break;
+    }
+  }
   return (
-    <div contentEditable={"true"} onKeyDown={e => {console.log}} className="bg-red-500 absolute w-32 h-32"></div> 
+    <div contentEditable onKeyDown={e => {console.log(e.code)}} className="bg-red-500 absolute w-32 h-32"></div> 
   )
 }
 {/* <input onKeyDown={ e => {console.log("KeyDown", e);} } type="text" /> */}
