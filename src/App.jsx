3a77a59e2f9a24345bf11187aeee4d62157f03d2
Marco D' Antino <marco.dantino@colidevs.com>
2026-04-17 23:29:42 -0300

    core coordinates

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,31 @@
 import { useState } from "react"
+//x horizontal y verttical 
 
 function App() {
-  const [coordinates, setCoordinates] = useState({x: 0, y: 0 })
+  const [coordinates, setCoordinates] = useState({x: 150, y: 150 })
 
   function handleOnKeyDown(eventCode){
     switch(eventCode){
-      case 'ArrowUp':
+      case "ArrowUp":
+        setCoordinates(prevCoordinates => ({ ...prevCoordinates, y: prevCoordinates.y-1 }))
+        console.log("ESTOY UP")
         break;
-      case 'ArrowDown':
+      case "ArrowDown":
+        setCoordinates(prevCoordinates => ({ ...prevCoordinates, y: prevCoordinates.y+1 }))
+        console.log("ESTOY DOWN")
         break;
-      case 'ArrowRight':
+      case "ArrowRight":
+        setCoordinates(prevCoordinates => ({...prevCoordinates, x: prevCoordinates.x-1 }))
+        console.log("ESTOY DERECHA")
         break;
-      case 'ArrowLeft':
+      case "ArrowLeft":
+        setCoordinates(prevCoordinates => ({ ...prevCoordinates, x: prevCoordinates.x+1  }))
+        console.log("ESTOY LEFT")
         break;
     }
   }
   return (
-    <div contentEditable onKeyDown={e => {console.log(e.code)}} className="bg-red-500 absolute w-32 h-32"></div> 
+    <div contentEditable onKeyDown={e => handleOnKeyDown(e.code) } style={{top: coordinates.y, right: coordinates.x}} className="bg-red-500 absolute w-32 h-32"></div> 
   )
 }
 {/* <input onKeyDown={ e => {console.log("KeyDown", e);} } type="text" /> */}
